88f6f1bbbe34d1356799c1aa32bf4fe5caecb298
Mariya Zaharieva <mariqtrunkova@gmail.com>
2023-11-11 16:00:34 +0200

    Initial components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,11 @@
 // import { useState } from 'react'
+import { OurClients } from './components/Clients/OurClients'
 import { Footer } from './components/Footer/Footer'
 import { Header } from './components/Header/Header'
 import { Hero } from './components/Hero/Hero'
+import { Products } from './components/Products/Products'
+import { Offer } from './components/Shop/Offer'
+import { Welcome } from './components/Welcome/Welcome'
 
 function App() {
   // const [count, setCount] = useState(0)
@@ -11,6 +15,10 @@ function App() {
       <Header />
       <main>
         <Hero/>
+        <Welcome/>
+        <Products/>
+        <Offer/>
+        <OurClients/>
       </main>
       <Footer />
     </>
