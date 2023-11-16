837122051f725471b57739a946500459ba4658c6
Mariya Zaharieva <mariqtrunkova@gmail.com>
2023-11-16 15:43:24 +0200

    Hero component update

--- client/src/components/Hero/Hero.jsx
+++ client/src/components/Hero/Hero.jsx
@@ -1,19 +1,16 @@
+import styles from '../Hero/Hero.module.css'
+
 export const Hero = () => {
     return (
         <div className="container-fluid bg-primary py-5 mb-5 hero-header">
         <div className="container py-5">
-            <div className="row justify-content-start">
-                <div className="col-lg-8 text-center text-lg-start">
+            <div className={styles.row}>
+                <div className={styles.col}>
                     {/* <h1 className="font-secondary text-primary mb-4">Вкусните торти</h1> */}
-                    <h1 className="display-1 text-uppercase text-white mb-4">Sistercake</h1>
-                    <h1 className="text-uppercase text-white">Приготвени с желание и любов</h1>
-                    <div className="d-flex align-items-center justify-content-center justify-content-lg-start pt-5">
+                    <h1 className="display-1">Sistercake</h1>
+                    <h2>Приготвени с желание и любов</h2>
+                    <div className={styles.button}>
                         <a href="" className="btn btn-primary border-inner py-3 px-5 me-5">Read More</a>
-                        {/* <button type="button" className="btn-play" data-bs-toggle="modal"
-                            data-src="https://www.youtube.com/embed/DWRcNpR6Kdc" data-bs-target="#videoModal">
-                            <span></span>
-                        </button> */}
-                        {/* <h5 className="font-weight-normal text-white m-0 ms-4 d-none d-sm-block">Play Video</h5> */}
                     </div>
                 </div>
             </div>
