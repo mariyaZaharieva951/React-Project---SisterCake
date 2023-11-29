693ca47e67ca6969accd0e46699f566c8b7f1b75
Mariya Zaharieva <mariqtrunkova@gmail.com>
2023-11-30 00:03:39 +0200

    Update MenuItem component

--- client/src/components/Products/MenuItem/MenuItem.jsx
+++ client/src/components/Products/MenuItem/MenuItem.jsx
@@ -1,28 +1,27 @@
 import { Link } from "react-router-dom";
+import styles from "../MenuItem/MenuItem.module.css";
 
-
-export const MenuItem = ({_id, imgUrl}) => {
-    return (
-        <div className="col-xl-3 col-lg-4 col-md-6 mb-4">
-                  <div className="bg-white rounded shadow-sm">
-                    <img
-                      src={imgUrl}
-                      alt=""
-                      className="img-fluid card-img-top"
-                    />
-                    <div className="p-4">
-                      <h5>
-
-                        <a href="#" className="text-dark">
-                          Red paint cup
-                        </a>
-                      </h5>
-                      {/* <p className="small text-muted mb-0">
+export const MenuItem = ({ _id, imgUrl }) => {
+  return (
+    <div className="col-xl-3 col-lg-4 col-md-6 mb-4">
+      <div className="bg-white rounded shadow-sm">
+        <img src={imgUrl} alt="" className="img-fluid card-img-top" />
+        <div className="p-4">
+          <div className={styles.text}>
+            <h5>
+              <Link to={`/${_id}`} className={styles.cakeTitle}>
+                Red paint cup
+              </Link>
+            </h5>
+            {/* <p className="small text-muted mb-0">
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit
                       </p> */}
-                      <button><Link to={`/${_id}`}>Детайли</Link></button>
-                    </div>
-                  </div>
-                </div>
-    )
-}
+            <div className={styles.btnItem}>
+              <Link to={`/${_id}`}>Детайли</Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
